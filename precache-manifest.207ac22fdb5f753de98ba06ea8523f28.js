4ce6a4716d0dbfc9cf2c18b5267637e39e6cb259
Hazel Feng <hazel@Hazels-MacBook-Air.local>
2020-07-10 14:18:57 -0700

    Updates

---
+++ precache-manifest.207ac22fdb5f753de98ba06ea8523f28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f901cf79a0dda10dae45ba352788aacc",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "c381d01b66439bbd0f85",
+    "url": "/calculator/static/css/main.e4323193.chunk.css"
+  },
+  {
+    "revision": "114bbaea14242877659e",
+    "url": "/calculator/static/js/2.6912228e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.6912228e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c381d01b66439bbd0f85",
+    "url": "/calculator/static/js/main.8253aec7.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
